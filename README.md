55e8a4f8bcd9b83d831ca84026125d3e2e2099b4
Larissa Gusmao <larissamgusmao@gmail.com>
2020-10-21 17:20:33 -0300

    build: setup react app using webpack

---
+++ README.md
@@ -0,0 +1,24 @@
+# Beer APP
+
+Project Roadmap:
+- [X] Use npm/yarn to manage app dependencies.
+- [X] Configure a React App using Webpack.
+- [ ] Create Components.
+- [ ] Display information from [API](https://punkapi.com/documentation/v2) using Axios to request the API.
+- [ ] Implement pagination.
+- [ ] Implement Unit and Functional tests using Jest.
+- [ ] Implement React Routes
+- [ ] Use Redux
+- [ ] Use Redux-Saga
+- [ ] Deploy APP
+
+## Installation
+
+Clone the repository and run command below to install dependencies
+```
+yarn install
+```
+## Start
+```
+yarn dev
+```
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const settings = {
+  publicPath: path.join(__dirname, "public"),
+  srcPath: path.join(__dirname, "src")
+};
+
+function srcPathExtend(subpath) {
+  return path.join(settings.srcPath, subpath)
+}
+
+module.exports = (env, options) => {
+  const isDevMode = options.mode === "development";
+
+  return {
+    devtool: isDevMode ? "source-map" : false,
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              sourceMap: isDevMode
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: [
+            { loader: 'style-loader' },
+            {
+              loader: 'css-loader', options: {
+                sourceMap: isDevMode
+              }
+            },
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: srcPathExtend("index.html")
+      })]
+  }
+}
