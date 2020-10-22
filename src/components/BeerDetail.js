79844d4cba6cc05caf5f4e2f873a30cc5ca405a6
Larissa Gusmao <larissamgusmao@gmail.com>
2020-10-22 01:32:40 -0300

    feat: display beer details

--- src/components/BeerDetail.js
+++ src/components/BeerDetail.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import { useParams } from "react-router";
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router"
+import api from '../services/api'
 
 const BeerDetail = () => {
   const { id } = useParams();
+  const [beer, setBeer] = React.useState({});
+
+  useEffect(() => {
+    let unmounted = false;
+    api.get(`beers/${id}`)
+      .then(res =>
+        setBeer({
+          name: res.data[0].name,
+          id: res.data[0].id,
+          tagline: res.data[0].tagline,
+          description: res.data[0].description,
+          image_url: res.data[0].image_url
+        })
+      )
+    return () => { unmounted = true };
+  }, [])
 
   return (
     <div>
-      Beer Detail {id}
+      <p>{beer.name}</p>
+      <p>{beer.tagline}</p>
+      <p>{beer.description}</p>
+      <p>{beer.image_url}</p>
     </div>
   )
 }
--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
 
-const BeerList = () => {
+import { fetchBeers } from '../store/actions/beersActions'
+
+const BeerList = ({ dispatch, loading, beers, hasErrors }) => {
+  useEffect(() => {
+    dispatch(fetchBeers())
+  }, [dispatch])
+
+  const renderBeers = () => {
+    if (loading) return <p>Loading...</p>
+    if (hasErrors) return <p>Unable to display beer list.</p>
+    return beers.map((beer) => <Link to={`/beer/${beer.id}`} key={beer.id}><p>{beer.name} - {beer.tagline}</p></Link>)
+  }
+
   return (
-    <div>
-      Beer List
-    </div>
+    <section>
+      <h1>Beers</h1>
+      {renderBeers()}
+    </section>
   )
 }
 
-export default BeerList
+const mapStateToProps = (state) => ({
+  loading: state.beers.loading,
+  beers: state.beers.beers,
+  hasErrors: state.beers.hasErrors,
+})
+
+export default connect(mapStateToProps)(BeerList)
